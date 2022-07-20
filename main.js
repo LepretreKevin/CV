8caa09bc852d7cf0eaac7f8c1a0d8cffa2b795cc
Lepretre Kevin <109674733+LepretreKevin@users.noreply.github.com>
2022-07-20 15:43:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const options = {
+  bottom: '90%', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '0px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#d0aea5', // default: '#fff'
+  backgroundColor: 'none',  // default: '#fff'
+  buttonColorDark: '#9a8385',  // default: '#100f2c'
+  buttonColorLight: '#FFFAF0', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '🌙', // default: ''
+  autoMatchOsTheme: true // default: true
+
+}
+
+
+
+
+
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
